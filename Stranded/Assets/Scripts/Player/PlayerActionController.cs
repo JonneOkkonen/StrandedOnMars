d79c2382e44094a1ef8f4b06ce484b7bf0982a98
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-03-25 00:29:43 +0200

    Add Gun Hide/UnHide Animation
    Hide/UnHide Gun with Q-key

---
+++ Stranded/Assets/Scripts/Player/PlayerActionController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActionController : MonoBehaviour
+{
+    public GameObject Gun;
+    Animator GunAnimator;
+    bool IsGunVisible;
+
+    void Awake() {
+        GunAnimator = Gun.GetComponent<Animator>();
+        Gun.SetActive(false);
+        GunAnimator.SetBool("IsVisible", false);
+        IsGunVisible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hide Gun with Q-key
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            if(IsGunVisible) {
+                // Disable Gun
+                StartCoroutine("DisableGun");
+                // Start Gun Hide Animation
+                GunAnimator.SetBool("IsVisible", false);
+                IsGunVisible = false;
+            }else {
+                // Enable Gun
+                Gun.SetActive(true);
+                // Start Gun UnHide Animation
+                GunAnimator.SetBool("IsVisible", true);
+                IsGunVisible = true;
+            }
+        }
+    }
+
+    // Disable Gun after 800ms
+    IEnumerator DisableGun() 
+    {
+        yield return new WaitForSeconds(.8f);
+        Gun.SetActive(false);
+    }
+}
