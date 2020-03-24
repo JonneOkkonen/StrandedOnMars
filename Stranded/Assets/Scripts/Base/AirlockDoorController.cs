af26e0f303e0bf5d9fb7d827ed09ba72102ccf59
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-03-24 18:44:55 +0200

    Add Airlock Door Logic
    Open/Close Airlock Doors with E-button when player is nearby

---
+++ Stranded/Assets/Scripts/Base/AirlockDoorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirlockDoorController : MonoBehaviour
+{
+    Animator Animator;
+    public bool IsOpen;
+    public bool PlayerNearby = false;
+    
+    void Awake()
+    {
+        Animator = GetComponent<Animator>();
+        Animator.SetBool("IsOpen", false);
+        IsOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Press E to Open/Close Door
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(IsOpen) {
+                CloseDoor();
+            }else {
+                OpenDoor();
+            }
+
+        }
+    }
+
+    // Open Door
+    void OpenDoor() {
+        if(PlayerNearby) {
+            Animator.SetBool("IsOpen", true);
+            IsOpen = true;
+        }
+    }
+
+    // Close Door
+    void CloseDoor() {
+        if(PlayerNearby) {
+            Animator.SetBool("IsOpen", false);
+            IsOpen = false;
+        }
+    }
+}
---
+++ Stranded/Assets/Scripts/Base/DoorTriggerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorTriggerController : MonoBehaviour
+{
+    public GameObject Door;
+    AirlockDoorController AirLock;
+    public GameObject ActionTextObject;
+    Text ActionText;
+
+    void Awake() {
+        AirLock = Door.GetComponent<AirlockDoorController>();
+        ActionText = ActionTextObject.GetComponent<Text>();
+    }
+
+    void Update() {
+        // Update Action Text
+        if(AirLock.IsOpen) {
+            ActionText.text = "Close (E)";
+        }else {
+            ActionText.text = "Open (E)";
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            // Enable Action Text
+            ActionTextObject.SetActive(true);
+            AirLock.PlayerNearby = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            // Disable Action Text
+            ActionTextObject.SetActive(false);
+            AirLock.PlayerNearby = false;
+        }
+    }
+}
