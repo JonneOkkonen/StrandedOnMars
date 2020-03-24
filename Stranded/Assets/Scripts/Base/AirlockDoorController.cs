c2a4e1398a1bd11fa2e5f1fae8362107767e4408
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-03-24 19:52:08 +0200

    Add Logic to Airlock
    Make sure its not possible to open both doors at the same time

--- Stranded/Assets/Scripts/Base/AirlockDoorController.cs
+++ Stranded/Assets/Scripts/Base/AirlockDoorController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AirlockDoorController : MonoBehaviour
 {
     Animator Animator;
     public bool IsOpen;
     public bool PlayerNearby = false;
+    public bool CanBeOpened = true;
+    public GameObject AnotherDoorObject;
+    AirlockDoorController AnotherDoor;
+    public GameObject InformationTextObject;
+    Text InformationText;
     
     void Awake()
     {
+        AnotherDoor = AnotherDoorObject.GetComponent<AirlockDoorController>();
+        InformationText = InformationTextObject.GetComponent<Text>();
         Animator = GetComponent<Animator>();
         Animator.SetBool("IsOpen", false);
         IsOpen = false;
@@ -18,6 +26,13 @@ public class AirlockDoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Change CanBeOpenedStatus based on anotherdoor status
+        if(AnotherDoor.IsOpen) {
+            CanBeOpened = false;
+        }else {
+            CanBeOpened = true;
+        }
+
         // Press E to Open/Close Door
         if(Input.GetKeyDown(KeyCode.E))
         {
@@ -32,14 +47,20 @@ public class AirlockDoorController : MonoBehaviour
 
     // Open Door
     void OpenDoor() {
-        if(PlayerNearby) {
+        // Make sure Player is nearby and door can be opened
+        if(PlayerNearby && CanBeOpened) {
             Animator.SetBool("IsOpen", true);
             IsOpen = true;
+            InformationTextObject.SetActive(false);
+        }else if(PlayerNearby && CanBeOpened == false) {
+            InformationTextObject.SetActive(true);
+            InformationText.text = "Close another door first";
         }
     }
 
     // Close Door
     void CloseDoor() {
+        // Make sure Player is nearby
         if(PlayerNearby) {
             Animator.SetBool("IsOpen", false);
             IsOpen = false;
