5c87e0938e10e7e6e9daf549a3933dd5dfc82634
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-03-25 17:50:45 +0200

    Add Rays/Light to Hologram
    Fix Plants

---
+++ Stranded/Assets/Scripts/Base/HologramRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramRotate : MonoBehaviour
+{
+    public int Speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * Speed, 0);
+    }
+}
