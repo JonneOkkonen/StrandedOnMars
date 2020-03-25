69ae1b1f46c6c4ec2b66caa226b76bebbb11b002
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-03-25 22:52:00 +0200

    Add Shooting Ability to Gun

---
+++ Stranded/Assets/Scripts/Player/BulletDestroy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    void Start() {
+        Destroy(this.gameObject, 2f);
+    }
+}
---
+++ Stranded/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject BulletSpawn;
+    Rigidbody BulletRB;
+    public int BulletSpeed;
+    public float GunCoolDown;
+    float Timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if(Input.GetButtonDown("Fire1") && Timer >= GunCoolDown) {
+            // Spawn Bullet
+            GameObject CurrentBullet = GameObject.Instantiate(Bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation);
+
+            // Get Bullet Rigidbody
+            BulletRB = CurrentBullet.GetComponent<Rigidbody>();
+            
+            // Shoot Bullet
+            BulletRB.velocity = -CurrentBullet.transform.up * BulletSpeed;
+
+            // Reset Timer
+            Timer = 0;
+        }
+    }
+}
