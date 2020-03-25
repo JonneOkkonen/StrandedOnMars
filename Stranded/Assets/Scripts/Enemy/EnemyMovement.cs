ba8f8e13a9cfdb446ffca0a6fc28c69a0ccbc0a3
M3268 <M3268@student.jamk.fi>
2020-03-25 16:40:42 +0200

    Added NavMesh and enemy pursuit of player
    //Added NavMesh to the Enemy characters, baked the NavMeshMap and made
    the enemy follow the player. Missing animation. Needs optimization.

---
+++ Stranded/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+	public Transform player;         // Reference to the player's position.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.	
+	
+	void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        nav = GetComponent <NavMeshAgent> ();
+    }
+	
+    void Update ()
+    {
+		nav.SetDestination(player.position);	
+		//nav.SetDestination (player.position);
+		//nav.enabled = false;
+    }
+}
