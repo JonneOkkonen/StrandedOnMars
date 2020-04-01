5a8a9fb910f7719ab3db56786777769f43de196c
M3268 <M3268@student.jamk.fi>
2020-04-01 15:50:13 +0300

    Added random melee and enemy shooting.
    //Added a randomizer to melee attack animation and enemy shooting.

--- Stranded/Assets/Scripts/Enemy/EnemyAttack.cs
+++ Stranded/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,17 +5,21 @@ using System.Collections;
 public class EnemyAttack : MonoBehaviour 
 {
 	
-	public float timeBetweenMelee = 0.5f;     // The time in seconds between each melee attack.
+	public float timeBetweenMelee = 5f;     // The time in seconds between each melee attack.
 	public float timeBetweenProjectile = 5f;  //The time in seconds between each projectile attack.
 	public int attackDamage = 10;               // The amount of health taken away per attack.
-	public int attackRange = 5;
+	public int meleeRange = 5;					//The range that the enemy GameObject can melee attack.
+	public int shootRange = 20;					//The range that the enemy GameObject can shoot attack.
+	public bool attackAbility; 					//Allows setting which attack is available for what enemy type.
 	
 	Animator anim;                              // Reference to the animator component.
 	GameObject player;                          // Reference to the player GameObject.
 	//PlayerHealth playerHealth;                  // Reference to the player's health.
 	//EnemyHealth enemyHealth;                    // Reference to this enemy's health.
-	bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+	bool playerInMeleeRange;                         // Whether player is within the trigger collider and can be attacked.
+	bool playerInShootRange;                         // Whether player is within the trigger collider and can be attacked.
 	float timer;                				// Timer counting next attack.
+	string[] melee = {"Stab Attack", "Smash Attack"}; //Create array to randomize between 2 attack animations.
 	
 	
 	void Awake () 
@@ -32,21 +36,39 @@ public class EnemyAttack : MonoBehaviour
 			// Add the time since Update was last called to the timer.
 			timer += Time.deltaTime;
 			float dist1 = Vector3.Distance(player.transform.position, transform.position);
-			Debug.Log(player.transform.position);
-			print("Distance to enemy: " + dist1);
+			float dist2 = Vector3.Distance(player.transform.position, transform.position);
+			//Debug.Log(player.transform.position);
+			//print("Distance to enemy: " + dist1);
 			
-			if(dist1 < attackRange)
+			if(dist1 < meleeRange)
 			{
-				playerInRange = true;
+				playerInMeleeRange = true;
 			}
 			
-			if(dist1 > attackRange) 
+			if(dist1 > meleeRange) 
 			{
-				playerInRange = false;
+				playerInMeleeRange = false;
 			}
-
+			
+			if(dist2 < shootRange) 
+			{
+				playerInShootRange = true;
+			}
+			
+			if(dist2 > shootRange) 
+			{
+				playerInShootRange = false;
+			}
+			
 			// If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
-			if(timer >= timeBetweenMelee && playerInRange /*&& enemyHealth.currentHealth > 0*/)
+			if(timer >= timeBetweenMelee && playerInMeleeRange /*&& enemyHealth.currentHealth > 0*/)
+			{
+				// ... attack.
+				Attack ();
+				
+			}
+			
+			if(timer >= timeBetweenProjectile && playerInShootRange /*&& enemyHealth.currentHealth > 0*/)
 			{
 				// ... attack.
 				Attack ();
@@ -66,7 +88,11 @@ public class EnemyAttack : MonoBehaviour
 		{
 			// Reset the timer.
 			timer = 0f;
-			anim.SetTrigger("Smash Attack");
+			
+			//Debug.Log("Returns value of melee: " + (melee[Random.Range(0, 2)]));
+			anim.SetTrigger(melee[Random.Range(0, 2)]);
+			
+			anim.SetTrigger("Cast Spell");
 
 			/*// If the player has health to lose...
 			if(playerHealth.currentHealth > 0)
