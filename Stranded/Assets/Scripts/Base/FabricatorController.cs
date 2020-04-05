39ab7a7b6c65706274c7c8ec666e50fa5ea0f218
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-04-05 18:37:26 +0300

    Create Controller for Fabricator

---
+++ Stranded/Assets/Scripts/Base/FabricatorController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FabricatorController : MonoBehaviour
+{
+    GameObject MainCamera;
+    GameObject FabricatorCamera;
+    public GameObject Player;
+    bool PlayerNearby = false;
+    public GameObject ActionTextObject;
+    Text ActionText;
+
+    void Awake()
+    {
+        // Get Both Cameras
+        MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        FabricatorCamera = GameObject.FindGameObjectWithTag("FabricatorCamera");
+        ActionText = ActionTextObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerNearby) {
+            if(Input.GetKeyDown(KeyCode.E)) {
+                SwitchCameras();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            // Update Action Text
+            ActionText.text = "Use Fabricator (E)";
+            // Enable Action Text
+            ActionTextObject.SetActive(true);
+            PlayerNearby = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            // Disable Action Text
+            ActionTextObject.SetActive(false);
+            PlayerNearby = false;
+        }
+    }
+
+    // Switch Cameras
+    void SwitchCameras() {
+        print("Switching cameras" + MainCamera.activeSelf);
+        if(MainCamera.activeSelf) {
+            Player.SetActive(false);
+            MainCamera.SetActive(false);
+            FabricatorCamera.SetActive(true);
+            // Disable Action Text
+            ActionTextObject.SetActive(false);
+        }else {
+            Player.SetActive(true);
+            FabricatorCamera.SetActive(false);
+            MainCamera.SetActive(true);
+        }
+    }
+}
--- Stranded/Assets/Scripts/Player/PlayerStats.cs
+++ Stranded/Assets/Scripts/Player/PlayerStats.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine.SceneManagement;
+using System.Globalization;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -19,6 +20,9 @@ public class PlayerStats : MonoBehaviour
     PlayerActionController PlayerActionController;
     public bool IsDead = false;
     public GameObject DeadScreen;
+    public int Points;
+    public GameObject PointsTextObject;
+    Text PointsText;
 
     void Awake()
     {
@@ -26,12 +30,16 @@ public class PlayerStats : MonoBehaviour
         OxygenBarText = OxygenBarObject.GetComponentInChildren(typeof(Text), true) as Text;
         PlayerController = GetComponent<RigidbodyFirstPersonController>();
         PlayerActionController = GetComponent<PlayerActionController>();
+        PointsText = PointsTextObject.GetComponent<Text>();
 
         // Set Oxygen to Max
         Oxygen = MaxOxygen;
 
         // Set Slider Max Value
         OxygenBar.maxValue = MaxOxygen;
+
+        // Set Points Text
+        SetPointsText(Points);
 
         // Start OxygenTick
         Invoke("OxygenTick", 1f);
@@ -84,5 +92,26 @@ public class PlayerStats : MonoBehaviour
         PlayerController.enabled = false;
         PlayerActionController.enabled = false;
         Debug.Log("Player has died");
+    }
+
+    // Use Points
+    public void UsePoints(int amount) {
+        Points -= amount;
+        // Update PointsText
+        SetPointsText(Points);
+    }
+
+    // Get Points
+    public void GetPoints(int amount) {
+        Points += amount;
+        // Update PointsText
+        SetPointsText(Points);
+    }
+
+    // Set Points Text
+    private void SetPointsText(int points) {
+        var nfi = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = " ";
+        PointsText.text = "Points: " + points.ToString("#,0", nfi);
     }
 }
